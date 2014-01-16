45b8d8e414232edc95628eaacf08a2d18bc68d01
Hack Reactor <hackreactor@HR-A2.local>
2014-01-15 22:59:47 -0800

    faster rook solution

--- src/solvers.js
+++ src/solvers.js
@@ -12,8 +12,7 @@
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n rooks placed such that none of them can attack each other
-window.count = 0;
-window.combos = {};
+
 
 window.findNRooksSolution = function(n){
   var solution = undefined; //fixme
@@ -31,48 +30,6 @@ window.findNRooksSolution = function(n){
   };
 
   placeRook();
-  // if (!board) {
-  //   var counter = counter || 0;
-  //   window.count = 0;
-  //   window.combos = {};
-  //   var matrix = [];
-  //   for (var i = 0; i < n; i++) {
-  //     matrix[i] = [];
-  //   }
-  //   for (var i = 0; i < n; i++) {
-  //     for (var j = 0; j<n; j++) {
-  //       matrix[i].push(0);
-  //     }
-  //   }
-  //   var newBoard = new Board(matrix);
-  // } else {
-  //   var newBoard = board;
-  // }
-
-  // var placeRook = function(){
-  //   //debugger;
-  //   for (var i = 0; i < n; i++) {
-  //     for (var j = 0; j < n; j++) {
-  //       if(!newBoard.get(i)[j]){ //if no rook exist at position j
-  //         newBoard.get(i)[j] = 1;
-  //         if (newBoard.hasAnyRooksConflicts()) {
-  //           newBoard.get(i)[j] = 0;
-  //         } else { //we've placed a rook
-  //           if (counter+1 === n) { //we've placed last rook
-  //             if(!window.combos.hasOwnProperty(JSON.stringify(newBoard._currentAttributes))){
-  //               window.count++;
-  //               window.combos[JSON.stringify(newBoard._currentAttributes)] = true;
-  //             }// return;
-  //           } else { //we need to place more rooks
-  //             findNRooksSolution(n, newBoard, counter+1);
-  //             newBoard.get(i)[j] = 0;
-  //           }
-  //         }
-  //       }
-  //     };
-  //   };
-  // };
-  // placeRook();
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return newBoard._currentAttributes[0];
@@ -82,52 +39,31 @@ window.findNRooksSolution = function(n){
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n){
-  var solutionCount = undefined; //fixme
+  var solutionsCount = 0;
   var solutions = [];
-  var newBoard = makeNewBoard(n);
+  var options = [];
 
-  var countNRooksSolutionsHelper = function(board, counter, rowIndex){
-    debugger;
-    var i = rowIndex;
+  for (var i = 0; i < n; i++) {
+    options.push(i);
+  }
 
-    // for (var i = 0; i < n; i++) {
-      for (var j = 0; j < n; j++) {
-        //place rook
-        if (board.get(i)[j] === 0) {
-          board.get(i)[j] = 1;
-          //if check failed
-          if (board.hasAnyRooksConflicts()) {
-            board.get(i)[j] = 0;
-          } else { //successfully added roook
-            counter++;
-            if(counter === n){ //if this is the last rook to add
-              solutions.push(JSON.stringify(board._currentAttributes));
-              board.get(i)[j] = 0;
-              counter--;
-            } else { //more rooks to add
-              countNRooksSolutionsHelper(board, counter, rowIndex +1);
-              board.get(i)[j] = 0;
-              counter--;
-            }
-          }
-        } else {
-          j = n; //go to next row
-        }
-      }
-    // }
-  };
-  // if (n === 1) {return 1;};
-  // if (n===2) {return 2;};
+  var innerFunction = function(history, count, choices){
+    if (count++ < n){
+      for (var i = 0; i < choices.length; i++) {
+        var limitedChoices = choices.slice();
+        var choice = limitedChoices.splice(i,1);
+        var newHistory = history.concat(choice);
+        innerFunction(newHistory, count, limitedChoices);
+      };
+    }
+    else {
+      solutionsCount++;
+      solutions.push(history);
+    }
+  }
 
-  // for (var i = 0; i < n; i++) {
-  //   newBoard.get(0)[i] = 1;
-  countNRooksSolutionsHelper(newBoard, 0, 0);
-  //   newBoard.get(0)[i] = 0;
-  // }
-  //countNRooksSolutionsHelper(newBoard, 0);
-  solutionCount = _.unique(solutions).length;
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  innerFunction([],0,options);
+  return solutionsCount;
 };
 
 window.makeNewBoard = function(n){
@@ -149,16 +85,128 @@ window.makeNewBoard = function(n){
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n){
   var solution = undefined; //fixme
+  var newBoard = makeNewBoard(n);
 
-  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
-  return solution;
+  var placeRook = function(){
+    for(i=0; i < n; i++){
+      for (var j = 0; j < n; j++) {
+        newBoard.get(i)[j] = 1;
+        if (newBoard.hasAnyQueensConflicts()) {
+          newBoard.get(i)[j] = 0;
+        }
+      };
+    }
+  };
+
+  placeRook();
+
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
+  return newBoard._currentAttributes[0];
 };
 
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n){
   var solutionCount = undefined; //fixme
+  var solutions = [];
+  var newBoard = makeNewBoard(n);
+  if (n === 0) return 0;
+  var countNQueensSolutionsHelper = function(board, counter, rowIndex){
+    debugger;
+    var i = rowIndex;
 
+    // for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        //place rook
+        if (board.get(i)[j] === 0) {
+          board.get(i)[j] = 1;
+          //if check failed
+          if (board.hasAnyQueensConflicts(j)) {
+            board.get(i)[j] = 0;
+          } else { //successfully added roook
+            counter++;
+            if(counter === n){ //if this is the last rook to add
+              solutions.push(JSON.stringify(board._currentAttributes));
+              board.get(i)[j] = 0;
+              counter--;
+            } else { //more rooks to add
+              countNQueensSolutionsHelper(board, counter, rowIndex +1);
+              board.get(i)[j] = 0;
+              counter--;
+            }
+          }
+        } else {
+          j = n; //go to next row
+        }
+      }
+    // }
+  };
+
+  countNQueensSolutionsHelper(newBoard, 0, 0);
+  //   newBoard.get(0)[i] = 0;
+  // }
+  //countNRooksSolutionsHelper(newBoard, 0);
+  solutionCount = _.unique(solutions).length;
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // var solutionCount = undefined; //fixme
+  // var solutions = [];
+  // var newBoard = makeNewBoard(n);
+
+  // var countNRooksSolutionsHelper = function(board, counter, rowIndex){
+  //   debugger;
+  //   var i = rowIndex;
+
+  //   // for (var i = 0; i < n; i++) {
+  //     for (var j = 0; j < n; j++) {
+  //       //place rook
+  //       if (board.get(i)[j] === 0) {
+  //         board.get(i)[j] = 1;
+  //         //if check failed
+  //         if (board.hasAnyColConflicts(j)) {
+  //           board.get(i)[j] = 0;
+  //         } else { //successfully added roook
+  //           counter++;
+  //           if(counter === n){ //if this is the last rook to add
+  //             solutions.push(JSON.stringify(board._currentAttributes));
+  //             board.get(i)[j] = 0;
+  //             counter--;
+  //           } else { //more rooks to add
+  //             countNRooksSolutionsHelper(board, counter, rowIndex +1);
+  //             board.get(i)[j] = 0;
+  //             counter--;
+  //           }
+  //         }
+  //       } else {
+  //         j = n; //go to next row
+  //       }
+  //     }
+  //   // }
+  // };
+  // // if (n === 1) {return 1;};
+  // // if (n===2) {return 2;};
+
+  // // for (var i = 0; i < n; i++) {
+  // //   newBoard.get(0)[i] = 1;
+  // countNRooksSolutionsHelper(newBoard, 0, 0);
+  // //   newBoard.get(0)[i] = 0;
+  // // }
+  // //countNRooksSolutionsHelper(newBoard, 0);
+  // solutionCount = _.unique(solutions).length;
+  // console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  // return solutionCount;
